ff0d816bc91559020f09fd4361ca5e2187cb409d
TerenzTuason <terenzjetcemtuason@gmail.com>
2025-04-28 22:20:43 +0800

    feat: change website logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ADMIN One Click Delivery - Tagum",
   description: "One Click Delivery - Tagum",
+  icons: {
+    icon: [
+      { url: '/logo/logo.jpg', sizes: 'any' }
+    ]
+  }
 };
 
 export default function RootLayout({
@@ -26,6 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="alternate icon" href="/logo/logo.jpg" type="image/jpeg" />
         <Script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
           strategy="beforeInteractive"
